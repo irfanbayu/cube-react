294c4835af6cfc02254040c0b6800ada827fe792
irfanbayu <setiawan.irfanbayu@gmail.com>
2022-06-06 21:03:32 +0700

    add environment, button

---
+++ src/scenes/Homepage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Button from 'elements/Button'
+
+export default function Homepage() {
+    return <div>
+        <Button isPrimary isSmall>
+            Sign Up
+        </Button>
+    </div>
+}
